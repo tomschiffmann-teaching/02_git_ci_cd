ddd0738aa778d55aedae20adfa0817e2fb935315
Tom Schiffmann <tom.schiffmann@it-accelerator.com>
2026-03-24 19:50:13 +0100

    add sg

--- main.tf
+++ main.tf
@@ -17,6 +17,24 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_security_group" "ssh" {
+  name        = "allow-ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "demo" {
   ami                    = "ami-0b6c6ebed2801a5cb" # ubuntu
